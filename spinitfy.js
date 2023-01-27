f1e5bdb5aa59113962a05adbb53b6e4c89292587
Catarina Conceição <catarinaconceic@gmail.com>
2023-01-27 17:21:04 +0000

    Implemented SPA system

--- spinitfy.js
+++ spinitfy.js
@@ -29,21 +29,68 @@ let repeat = false;
 let shuffle = false;
 let updateTimer;
 
-function mainModuleReset() {
-  mainModule.innerHTML = "";
-  mainModule.classList.remove("home");
-  mainModule.classList.remove("search");
-  mainModule.classList.remove("library");
-}
-const randColor = () => {
-  return (
-    "#" +
-    Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, "0")
-      .toUpperCase()
-  );
+/////////////////////////////////////////////////////////////////////URL ROUTING
+const urlPageTitle = "Spinitfy Web Player";
+
+document.addEventListener("click", (e) => {
+  const { target } = e;
+  if (!target.matches("nav a")) {
+    return;
+  }
+  e.preventDefault();
+  urlRoute();
+});
+
+const urlRoutes = {
+  404: {
+    template: "/templates/404.html",
+    title: "404 | " + urlPageTitle,
+    description: "Page not found",
+  },
+  "/": {
+    template: "/templates/home.html",
+    title: urlPageTitle + " | Home",
+    description: "Home page",
+  },
+  "/search": {
+    template: "/templates/search.html",
+    title: urlPageTitle + " | Search",
+    description: "Search page",
+  },
+  "/library": {
+    template: "/templates/library.html",
+    title: urlPageTitle + " | Library",
+    description: "Library page",
+  },
 };
+
+const urlRoute = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  urlLocationHandler();
+};
+
+const urlLocationHandler = async () => {
+  const location = window.location.pathname;
+  if (location.length == 0) {
+    location = "/";
+  }
+  const route = urlRoutes[location] || urlRoutes["404"];
+  const html = await fetch(route.template).then((response) => response.text());
+  document.querySelector(".main-div").innerHTML = html;
+  document.title = route.title;
+  document
+    .querySelector('meta[name="description"]')
+    .setAttribute("content", route.description);
+};
+
+window.onpopstate = urlLocationHandler;
+window.route = urlRoute;
+urlLocationHandler();
+
+/////////////////////////////////////////////////////////////////////Media Player
+
 const songList = [
   {
     tittle: "Forest Lullaby",
